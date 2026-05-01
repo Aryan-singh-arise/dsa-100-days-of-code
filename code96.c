a76bb694f78134b8195daf1fe8c6ef67f78c0c44
aryan singh <ar9897592@gmail.com>
2026-05-01 10:20:45 +0530

    comit

---
+++ code96.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;      // left subarray
+    int j = mid + 1;   // right subarray
+    int k = left;      // temp index
+    long long inv_count = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i + 1);  // all remaining in left are greater
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+
+    for (int p = left; p <= right; p++) {
+        arr[p] = temp[p];
+    }
+
+    return inv_count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inv_count = 0;
+
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        inv_count += mergeSort(arr, temp, left, mid);
+        inv_count += mergeSort(arr, temp, mid + 1, right);
+
+        inv_count += merge(arr, temp, left, mid, right);
+    }
+
+    return inv_count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int temp[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    long long result = mergeSort(arr, temp, 0, n - 1);
+
+    printf("%lld\n", result);
+
+    return 0;
+}
