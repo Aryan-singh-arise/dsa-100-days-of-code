61df331f47988d591f434a9f80b84ad7db6fc716
aryan singh <ar9897592@gmail.com>
2026-05-01 09:57:15 +0530

    comit

---
+++ code88.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int canPlace(int stalls[], int n, int k, int dist) {
+    int count = 1;
+    int lastPos = stalls[0];
+
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - lastPos >= dist) {
+            count++;
+            lastPos = stalls[i];
+        }
+    }
+
+    return count >= k;
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int stalls[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &stalls[i]);
+    }
+
+    sort(stalls, n);
+
+    int low = 1;
+    int high = stalls[n - 1] - stalls[0];
+    int ans = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (canPlace(stalls, n, k, mid)) {
+            ans = mid;       // valid distance
+            low = mid + 1;   // try bigger
+        } else {
+            high = mid - 1;   // try smaller
+        }
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
